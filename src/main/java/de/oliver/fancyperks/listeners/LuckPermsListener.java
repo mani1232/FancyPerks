f9909495516d8d49053e6963f42fd8701dfb57ca
mani123 <70480001+mani1232@users.noreply.github.com>
2023-05-19 17:00:52 +0200

    Fix LP remove perm

--- src/main/java/de/oliver/fancyperks/listeners/LuckPermsListener.java
+++ src/main/java/de/oliver/fancyperks/listeners/LuckPermsListener.java
@@ -6,6 +6,7 @@ import de.oliver.fancyperks.perks.Perk;
 import de.oliver.fancyperks.perks.PerkRegistry;
 import net.luckperms.api.event.EventBus;
 import net.luckperms.api.event.node.NodeAddEvent;
+import net.luckperms.api.event.node.NodeRemoveEvent;
 import net.luckperms.api.model.user.User;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -15,6 +16,7 @@ public class LuckPermsListener {
     public LuckPermsListener() {
         EventBus eventBus = FancyPerks.getInstance().getLuckPerms().getEventBus();
         eventBus.subscribe(FancyPerks.getInstance(), NodeAddEvent.class, this::onNodeAdd);
+        eventBus.subscribe(FancyPerks.getInstance(), NodeRemoveEvent.class, this::onNodeRemove);
     }
 
     private void onNodeAdd(NodeAddEvent event) {
@@ -37,17 +39,42 @@ public class LuckPermsListener {
             }
 
             if (event.getNode().getValue() && p.hasPermission("fancyperks.perk." + perk.getSystemName())) {
-                perk.grant(p);
-                MessageHelper.success(p, "Automatically enabled the " + perk.getDisplayName() + " perk");
-            } else if (!p.hasPermission("fancyperks.perk." + perk.getSystemName())) {
+                FancyPerks.getInstance().getScheduler().runTask(p.getLocation(), () -> {
+                    perk.grant(p);
+                    MessageHelper.success(p, "Automatically enabled the " + perk.getDisplayName() + " perk");
+                });
+            }
+        }
+
+    }
+
+    private void onNodeRemove(NodeRemoveEvent event) {
+        String permission = event.getNode().getKey().toLowerCase();
+        if (!permission.startsWith("fancyperks.perk.")) {
+            return;
+        }
+
+        String perkStr = permission.substring(permission.lastIndexOf('.') + 1, permission.length());
+        Perk perk = PerkRegistry.getPerkByName(perkStr);
+        if (perk == null) {
+            return;
+        }
+
+        if (event.isUser()) {
+            User user = (User) event.getTarget();
+            Player p = Bukkit.getPlayer(user.getUniqueId());
+            if (p == null) {
+                return;
+            }
+
+            if (!p.hasPermission("fancyperks.perk." + perk.getSystemName())) {
                 MessageHelper.warning(p, "Perk " + perk.getDisplayName() + " turns off after 15 seconds");
-                FancyPerks.getInstance().getScheduler().runTaskLater(p.getLocation(),15, () -> {
+                FancyPerks.getInstance().getScheduler().runTaskLater(p.getLocation(), 15 * 20, () -> {
                     perk.revoke(p);
                     MessageHelper.success(p, "Automatically disabled the " + perk.getDisplayName() + " perk");
                 });
             }
         }
-
     }
 
 }
